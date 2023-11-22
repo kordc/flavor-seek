e653bd678d2128cac01a167aa618128822d87e6d
SebChw <sebastian.chwilczynski@gmail.com>
2023-11-22 15:14:13 +0100

    precalculated graph embeddings

---
+++ scripts/create_graph_embeddings.py
@@ -0,0 +1,47 @@
+"""After training graph embeddings network we can use it to create embeddings for recipes.
+"""
+
+import argparse
+import json
+from pathlib import Path
+
+import numpy as np
+import polars as pl
+from eatpim.rank_subs_in_recipe import content_to_ids, load_embedding_data
+
+# Allows whole dish embedding calculation
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_dir", type=str, required=True)
+parser.add_argument("--model_dir", type=str, required=True)
+args = parser.parse_args()
+main_dir = "data" / Path(args.data_dir)
+embedding_calculator = calc = load_embedding_data(main_dir, args.model_dir)
+with open((main_dir / "ingredient_list.json").resolve(), "r") as f:
+    ingredient_list = json.load(f)
+
+
+# I need mapping from Dish to embedding
+recipes_file = (main_dir / "recipe_tree_data.json").resolve()
+with open(recipes_file, "r") as f:
+    recipe_data = json.load(f)
+
+indices = []
+embeddings = []
+for dset in ["train.txt", "valid.txt", "test.txt"]:
+    with open((main_dir / f"eatpim_triple_data/{dset}").resolve()) as fin:
+        for line in fin:
+            graph_dict = json.loads(line)
+            # there should only be one item in the first depth of this dict
+            # the key is the output recipe node, the value is the dictionary representation of the flowgraph
+            for k, v in graph_dict.items():
+                indices.append(int(k.split("_")[-1]))
+                embeddings.append(
+                    embedding_calculator.GOpTranseCalcOperation(
+                        ops=content_to_ids(v), rem_ing=None
+                    )
+                )
+
+indices = np.array(indices)
+embeddings = np.array(embeddings)
+df = pl.DataFrame(data={"recipe_id": indices, "embedding": embeddings})
+df.write_parquet("recipe_embeddings.parquet")
